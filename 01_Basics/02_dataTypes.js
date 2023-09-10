e55cc8122f37f39bf6cd40c0a4975a777bff6827
Jitendra Shekhawat <106915040+Shekhawat-J@users.noreply.github.com>
2023-09-10 19:33:31 +0000

    Data Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // Treat all JS Code as newer version
+
+//alert("Hello");  
+/*
+// we are using node js, not browser
+// will get alert is not defined 
+// if you use alert in browser console then will run becuase brower having js engine 
+//But in node we have different approch to use the alret()
+*/
+
+let name = "Jitendra"; //String
+let age = 18; //Number
+let isLoggedIn = false //Boolean 
+
+/*
+    Number => 2^53
+    BigInt
+    String --> single quotes or double quotes but prefered double quotes 
+    boolean  --> True/False
+    null --> Standalone value  (It's representation of empty)
+    undegined --> we didn't defined/assign the value
+    symbol --> uniuqe 
+
+
+    Object 
+
+*/
+
+console.log(typeof "Jitendra"); //string
+console.log(typeof age); //number
+console.log(typeof null);  //Object
+console.log(typeof undefined); //undefined
+
