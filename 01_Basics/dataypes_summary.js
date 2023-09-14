8653a3eb1f710d04199272a1add7076ebf59258f
Jitendra Shekhawat <106915040+Shekhawat-J@users.noreply.github.com>
2023-09-14 11:14:38 +0000

    data types summary

---
+++ 01_Basics/dataypes_summary.js
@@ -0,0 +1,11 @@
+/*
+ Primitive : these are call by value type of data that means whenever we are coping these data types value then we will get the copy of these value not the referecne 
+ 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+*/
+
+/*
+Reference type or Non Primitive type
+these are the value for which we can get the reference of these value 
+Array, Objects, Functions
+
+*/
